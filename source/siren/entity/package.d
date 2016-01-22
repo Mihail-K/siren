8f1efbed872b52321d0852649da7d2425d1dd835
Mihail-K <Mihail.K@fimu.tk>
2016-01-21 20:27:46 -0500

    Add package file.

---
+++ source/siren/entity/package.d
@@ -0,0 +1,8 @@
+
+module siren.entity;
+
+public import siren.entity.attributes;
+public import siren.entity.base;
+public import siren.entity.column;
+public import siren.entity.id;
+public import siren.entity.transient;
