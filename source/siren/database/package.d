ab922ce885007679c5adf2b0da5c6f7536f21d6f
Mihail-K <Mihail.K@fimu.tk>
2016-01-21 21:46:43 -0500

    Add package file.

---
+++ source/siren/database/package.d
@@ -0,0 +1,7 @@
+
+module siren.database;
+
+public import siren.database.adapter;
+public import siren.database.insert_result;
+public import siren.database.query_result;
+public import siren.database.query_row;
