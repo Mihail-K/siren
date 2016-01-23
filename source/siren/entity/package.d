12c3a9b5cd7ec9b58eba29e35821e97df9d83624
Mihail-K <Mihail.K@fimu.tk>
2016-01-23 18:13:39 -0500

    Fix missing import.

--- source/siren/entity/package.d
+++ source/siren/entity/package.d
@@ -5,4 +5,5 @@ public import siren.entity.attributes;
 public import siren.entity.base;
 public import siren.entity.column;
 public import siren.entity.id;
+public import siren.entity.table;
 public import siren.entity.transient;
