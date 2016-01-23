c74ca2fa15851274fa110d3dc7d192ae6516053d
Mihail-K <Mihail.K@fimu.tk>
2016-01-23 18:04:08 -0500

    Add package file.

---
+++ source/siren/sirl/package.d
@@ -0,0 +1,8 @@
+
+module siren.sirl;
+
+public import siren.sirl.generator;
+public import siren.sirl.node;
+public import siren.sirl.node_visitor;
+public import siren.sirl.select_builder;
+public import siren.sirl.table;
