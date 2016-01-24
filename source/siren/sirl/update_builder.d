b269a3d0b6edfd2545a03c911f0943ba46eb8495
Mihail-K <Mihail.K@fimu.tk>
2016-01-24 16:16:30 -0500

    Add set and where functions for update builder.

--- source/siren/sirl/update_builder.d
+++ source/siren/sirl/update_builder.d
@@ -23,9 +23,65 @@ public:
         _table = table.table;
     }
 
+    UpdateBuilder set(T)(T sets)
+    if(is(T : AssignNode) || is(T : AssignNode[]))
+    {
+        if(_update.set is null)
+        {
+            _update.set = new SetNode(sets);
+        }
+        else
+        {
+            _update.set ~= sets;
+        }
+
+        return this;
+    }
+
+    UpdateBuilder set()(FieldNode left, ExpressionNode right)
+    {
+        return this.set(left = right);
+    }
+
+    UpdateBuilder set(T, V)(T field, V value)
+    {
+        auto left = FieldNode.create(_table, field);
+        auto right = LiteralNode.create(value);
+
+        return this.set(left, right);
+    }
+
     @property
     string table()
     {
         return _table;
+    }
+
+    UpdateBuilder where(T)(T clauses)
+    if(is(T : ExpressionNode) || is(T : ExpressionNode[]))
+    {
+        if(_update.where is null)
+        {
+            _update.where = new WhereNode(clauses);
+        }
+        else
+        {
+            _update.where ~= clauses;
+        }
+
+        return this;
+    }
+
+    UpdateBuilder where()(FieldNode left, ExpressionNode right)
+    {
+        return this.where(left.eq(right));
+    }
+
+    UpdateBuilder where(T, V)(T field, V value)
+    {
+        auto left = FieldNode.create(_table, field);
+        auto right = LiteralNode.create(value);
+
+        return this.where(left, right);
     }
 }
