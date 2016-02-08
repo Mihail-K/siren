9d3e7d1a43f9097ca1a74fc5c0969af62c5582d0
Mihail-K <Mihail.K@fimu.tk>
2016-02-07 20:48:25 -0500

    Fix adapter provider token issue.

--- source/siren/database/adapter_provider.d
+++ source/siren/database/adapter_provider.d
@@ -14,7 +14,7 @@ final class AdapterProvider
      ++/
     static final class Token
     {
-        private this()
+        private shared this()
         {
             // Hidden constructor.
         }
@@ -33,7 +33,7 @@ final class AdapterProvider
 private:
     static Adapter[string] _cachedAdapters;
     static ClassInfo[string] _registeredAdapters;
-    static string[Token] _tokens;
+    static string[shared(Token)] _tokens;
 
 public:
     /++
@@ -81,18 +81,18 @@ public:
      + Registers an adapter to the given name using a template type.
      + A token is issued, which can be used to unregister the adapter.
      ++/
-    static Token register(A : Adapter)(string name)
+    static shared(Token) register(A : Adapter)(string name)
     {
         enum typename = fullyQualifiedName!A;
-        return register(name, ClassInfo.find(typename));
+        return register(name, cast(ClassInfo) ClassInfo.find(typename));
     }
 
-    private static Token register()(string name, ClassInfo class_)
+    private static shared(Token) register()(string name, ClassInfo class_)
     {
         enforce(class_, "Cannot register adapter with null class.");
         enforce(name !in _registeredAdapters, "Adapter `" ~ name ~ "` already registered.");
 
-        auto token = new Token;
+        auto token = new shared Token;
         _registeredAdapters[name] = class_;
         _tokens[token] = name;
 
@@ -102,7 +102,7 @@ public:
     /++
      + Unregisters an adapter, using a previously issued token.
      ++/
-    static void unregister(Token token)
+    static void unregister(shared(Token) token)
     {
         auto namePtr = token in _tokens;
         enforce(namePtr !is null, "No Adapter registered to Token.");
