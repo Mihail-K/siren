4dd79ab4cc2b7171196e28f1d1704b82aecefcc1
Mihail-K <Mihail.K@fimu.tk>
2016-02-10 22:43:28 -0500

    Exception handling for basic model functions.

--- source/siren/model/base.d
+++ source/siren/model/base.d
@@ -3,17 +3,23 @@ module siren.model.base;
 
 import siren.database;
 import siren.entity;
+import siren.model.exception;
 import siren.sirl;
 import siren.util;
 
 import std.conv;
 import std.exception;
+import std.typecons;
+import std.variant;
 
 class Model(E : Entity)
 {
 private:
     static Adapter _adapter;
     static Query _table;
+
+    // Ensure the Entity has an ID column defined.
+    static assert(hasID!E, "Entity `" ~ E.stringof ~ "` is missing an @ID column.");
 
 public:
     @property
@@ -42,34 +48,38 @@ public:
         // Fire before entity-create callbacks.
         fire!(CallbackEvent.BeforeCreate)(entity);
 
+        // Run the insert query and fetch the result.
         auto result = adapter.insert(query, E.stringof);
+
+        // Set the entity ID from the query result.
+        Nullable!Variant id = Variant(result.lastInsertID.get);
+        set!(E, getIDColumnField!E)(entity, id);
 
         // Fire after entity-create callbacks.
         fire!(CallbackEvent.AfterCreate)(entity);
 
-        return true;
+        // Ensure changes were made.
+        return result.count != 0;
     }
 
     bool destroy(E entity)
     {
-        auto id = get(entity, getIDColumnField!E);
-
-        auto columns = getNonIDColumnNames!E;
-        auto values = get(entity, columns);
-
         auto query = table.destroy
-            .where(getIDColumnName!E, id);
+            .where(getIDColumnName!E, getID(entity));
 
         // Fire before entity-destroy callbacks.
         fire!(CallbackEvent.BeforeDestroy)(entity);
 
+        // Run the delete query and fetch the result.
         auto result = adapter.destroy(query, E.stringof);
-        enforce(result != 0, "Entity `" ~ E.stringof ~ "` with id `" ~ id.text ~ "` not destroyed.");
+
+        // Ensure that the entity was removed from the database.
+        enforce(result != 0, EntityNotDestroyedException.create(entity));
 
         // Fire after entity-destroy callbacks.
         fire!(CallbackEvent.AfterDestroy)(entity);
 
-        return true;
+        return result != 0;
     }
 
     static E find(IDType!E id)
@@ -79,8 +89,11 @@ public:
             .where(getIDColumnName!E, id)
             .limit(1);
 
+        // Try to fetch the entity from the database.
         auto result = adapter.select(query, E.stringof);
-        enforce(!result.empty, "No " ~ E.stringof ~ " with id `" ~ id.text ~ "` found.");
+
+        // Ensure that something was found by the query.
+        enforce(!result.empty, EntityNotFoundException.create!(E)(id));
 
         auto entity = new E;
         auto row = result.front;
@@ -98,17 +111,25 @@ public:
         }
     }
 
+    Nullable!Variant getID(E entity)
+    {
+        return get!(E, getIDColumnField!E)(entity);
+    }
+
     @property
     static Query table()
     {
         if(_table is null)
         {
             // Use lazy initialization.
-            _table = new Query(getTableName!E);
+            _table = new Query(tableName);
         }
 
         return _table;
     }
+
+    @property
+    enum string tableName = getTableName!E;
 
     private bool update(E entity)
     {
@@ -116,21 +137,26 @@ public:
         // TODO : Run validations.
         // TODO : Run callbacks.
 
-        auto id = get(entity, getIDColumnField!E);
-
         auto columns = getNonIDColumnNames!E;
         auto values = get(entity, columns);
 
         auto query = table.update
-            .where(getIDColumnName!E, id)
+            .where(getIDColumnName!E, getID(entity))
             .set(columns, values);
 
-        // TODO : Run callbacks.
+        // Fire before entity-update callbacks.
+        fire!(CallbackEvent.BeforeUpdate)(entity);
 
+        // Run the delete query and fetch the result.
         auto result = adapter.update(query, E.stringof);
 
-        // TODO : Run callbacks.
+        // Ensure that the entity was updated in the database.
+        enforce(result != 0, EntityNotUpdatedException.create(entity));
 
-        return true;
+        // Fire after entity-update callbacks.
+        fire!(CallbackEvent.AfterUpdate)(entity);
+
+        // Ensure changes were made.
+        return result != 0;
     }
 }
---
+++ source/siren/model/exception.d
@@ -0,0 +1,50 @@
+
+module siren.model.exception;
+
+import siren.entity;
+import siren.exception;
+
+import std.conv;
+
+class EntityNotDestroyedException : SirenException
+{
+    this(string entity, string id)
+    {
+        super("Entity `" ~ entity ~ "` with ID `" ~ id ~ "` not destroyed.");
+    }
+
+    static auto create(E : Entity)(E entity)
+    {
+        auto id = get!(E, getIDColumnField!E)(entity);
+
+        return new EntityNotDestroyedException(E.stringof, id.text);
+    }
+}
+
+class EntityNotFoundException : SirenException
+{
+    this(string entity, string id)
+    {
+        super("Entity `" ~ entity ~ "` with ID `" ~ id ~ "` not found.");
+    }
+
+    static auto create(E : Entity)(IDType!E id)
+    {
+        return new EntityNotFoundException(E.stringof, id.text);
+    }
+}
+
+class EntityNotUpdatedException : SirenException
+{
+    this(string entity, string id)
+    {
+        super("Entity `" ~ entity ~ "` with ID `" ~ id ~ "` not updated.");
+    }
+
+    static auto create(E : Entity)(E entity)
+    {
+        auto id = get!(E, getIDColumnField!E)(entity);
+
+        return new EntityNotUpdatedException(E.stringof, id.text);
+    }
+}
