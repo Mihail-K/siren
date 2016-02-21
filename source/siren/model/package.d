fcf39df5e1fd96b52fbc537eff7f9a0060a7ad77
Mihail-K <Mihail.K@fimu.tk>
2016-02-20 20:10:38 -0500

    Remove old model code.

--- source/siren/model/package.d
+++
@@ -1,6 +0,0 @@
-
-module siren.model;
-
-public import siren.model.base;
-public import siren.model.exception;
-public import siren.model.relation;
--- source/siren/package.d
+++ source/siren/package.d
@@ -7,7 +7,6 @@ public import siren.database;
 public import siren.entity;
 public import siren.exception;
 public import siren.logger;
-public import siren.model;
 public import siren.relation;
 public import siren.schema;
 public import siren.sirl;
